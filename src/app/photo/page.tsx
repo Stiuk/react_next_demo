292ed4f0fa1b7d34d847bc593718713af6fff557
liuke <liuke@56lcy.com>
2025-07-25 11:23:14 +0800

    1

--- src/app/photo/page.tsx
+++ src/app/photo/page.tsx
@@ -1,5 +1,6 @@
 export default async function Page () {
     await new Promise<string>((resolve) => {
+      "use server";
       console.log('in executing sleep!')
       setTimeout(() => resolve("after 3000 ms!"), 3000);
     });
